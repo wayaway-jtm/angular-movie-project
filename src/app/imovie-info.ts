5c517f01ab1e5115017115d94d7853686b4f38e1
john <wayaway.jtm@gmail.com>
2020-02-01 14:11:48 -0500

    uncommented length cuz I was mistaken

--- src/app/imovie-info.ts
+++ src/app/imovie-info.ts
@@ -8,8 +8,6 @@ export interface IMovieInfo {
         releaseDate: string;
         rating: string; // Is this supposed to be popularity or vote_average?
         genreIDs: number[];
-        // length: string; API doesn't include movie length.
-        //                  We'd have to implement IMDB or some
-        //                  other API for that.
+        length: string;
     }
 }
