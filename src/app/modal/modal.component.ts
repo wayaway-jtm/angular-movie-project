185b9fdd12ae8daa69623a66687cb93183222ef9
chase <chasebaker21@gmail.com>
2020-02-05 20:53:51 -0500

    removed commented out code

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -7,26 +7,4 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent {
 
-//   closeResult: string;
-
-//   constructor(private modalService: ModalComponent) {}
-
-//   open(content, any) {
-//     this.modalService.open(content, {ariaLabelledBy: 'modal-title'}).result.then((result) => {
-//       this.closeResult = `Closed with: ${result}`;
-//     }, (reason) => {
-//       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-//     });
-//   }
-
-//   private getDismissReason(reason: any): string {
-//     if (reason === ModalDismissReasons.ESC) {
-//       return 'by pressing ESC';
-//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-//       return 'by clicking on a backdrop';
-//     } else {
-//       return  `with: ${reason}`;
-//     }
-//   }
-// }
 }
